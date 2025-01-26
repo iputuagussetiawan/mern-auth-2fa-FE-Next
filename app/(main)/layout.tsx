16adbaedb3d0590f1830521caface0619ce54655
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2025-01-26 13:28:46 +0800

    multi factor auth modul with google autenticator scane qrcode

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,3 +1,20 @@
-export default function MainLayout({ children }: { children: React.ReactNode }) {
-    return <div>{children}</div>;
+import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
+import Header from "./_components/Header";
+import Asidebar from "./_components/Asidebar";
+import { AuthProvider } from "@/context/auth-provider";
+
+export default function MainLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+    return (
+        <AuthProvider>
+            <SidebarProvider>
+                <Asidebar />
+                <SidebarInset>
+                    <main className="w-full">
+                        <Header />
+                        {children}
+                    </main>
+                </SidebarInset>
+            </SidebarProvider>
+        </AuthProvider>
+    );
 }
