41d06bf66e512c88dd4b281a939b365753c1bfd4
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2025-01-26 13:49:06 +0800

    revoke mfa module

--- app/(main)/_components/_common/RevokeMfa.tsx
+++ app/(main)/_components/_common/RevokeMfa.tsx
@@ -11,20 +11,20 @@ const RevokeMfa = () => {
     const { mutate, isPending } = useMutation({
         mutationFn: revokeMFAMutationFn,
         onSuccess: (response: any) => {
-        queryClient.invalidateQueries({
-            queryKey: ["authUser"],
-        });
-        toast({
-            title: "Success",
-            description: response.message,
-        });
+            queryClient.invalidateQueries({
+                queryKey: ["authUser"],
+            });
+            toast({
+                title: "Success",
+                description: response.message,
+            });
         },
         onError: (error: any) => {
-        toast({
-            title: "Error",
-            description: error.message,
-            variant: "destructive",
-        });
+            toast({
+                title: "Error",
+                description: error.message,
+                variant: "destructive",
+            });
         },
     });
 
@@ -34,12 +34,12 @@ const RevokeMfa = () => {
 
     return (
         <Button
-        disabled={isPending}
-        className="h-[35px] !text-[#c40006d3] !bg-red-100 shadow-none mr-1"
-        onClick={handleClick}
-        >
-        {isPending && <Loader className="animate-spin" />}
-        Revoke Access
+            disabled={isPending}
+            className="h-[35px] !text-[#c40006d3] !bg-red-100 shadow-none mr-1"
+            onClick={handleClick}
+            >
+            {isPending && <Loader className="animate-spin" />}
+            Revoke Access
         </Button>
     );
 };
